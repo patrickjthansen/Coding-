72cef641984e93565c5fff006b51c7d3bbb7c655
Patrick James Hansen <71075559+patrickjthansen@users.noreply.github.com>
2020-09-18 11:50:48 +0200

    Update HalloVerden.java
    Synes det er greit å med været i dag

--- HalloVerden.java
+++ HalloVerden.java
@@ -3,5 +3,6 @@ package halloVerden;
 public class HalloVerden {
     public static void main(String [] args){
         System.out.println("Hallo Verden");
+        System.out.println("Det er fint vær i dag")
     }
 }
